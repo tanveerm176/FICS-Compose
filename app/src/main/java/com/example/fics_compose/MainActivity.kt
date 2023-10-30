0881afd3be9411c402496f873ebada4dce618bef
fatimajavid <fatimajavid101@gmail.com>
2023-10-30 10:44:08 -0400

    Splash Screen

--- app/src/main/java/com/example/fics_compose/MainActivity.kt
+++ app/src/main/java/com/example/fics_compose/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.fics_compose.ui.theme.FICSComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             FICSComposeTheme {
                 MainScreen()
