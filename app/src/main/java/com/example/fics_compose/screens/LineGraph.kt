fbcfcbe1d37882afb9f64aa3182c12641abcf57c
tanveerm176 <tanveerm176@gmail.com>
2023-12-17 17:28:31 -0500

    generated line graph composable

--- app/src/main/java/com/example/fics_compose/screens/LineGraph.kt
+++ app/src/main/java/com/example/fics_compose/screens/LineGraph.kt
@@ -4,26 +4,39 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 data class AssestInfo(
     val netWorthList: List<Double>
 )
 
-val LightOlive = Color(0xFFacc8b2)
-val LightCarmin = Color(0xFFe7978b)
+val LightOlive = Color(0xFF28A72D)
+val LightCarmin = Color(0xFFF44336)
 
 @Composable
-fun PerfomanceChart(modifier: Modifier = Modifier, list: List<Double> = mockData.netWorthList) {
+@Preview(heightDp = 300, widthDp = 300, backgroundColor = 0xFFFFFFFF, showBackground = true)
+fun PerformanceChart(modifier: Modifier = Modifier, list: List<Double> = mockData.netWorthList) {
     var floatList: List<Float> = list.map { it.toFloat() }
 
     val zipList: List<Pair<Float, Float>> = floatList.zipWithNext()
-Card {
+Card(
+
+    modifier = Modifier
+        .wrapContentHeight()
+        .fillMaxWidth()
+        .padding(start = 5.dp, end = 5.dp, bottom = 5.dp),
+    colors = CardDefaults.cardColors(containerColor = Color.LightGray)
+) {
     Row {
         val max = floatList.max()
         val min = floatList.min()
@@ -37,17 +50,18 @@ Card {
 
             Canvas(
                 modifier = Modifier
+                    .weight(1f)
                     .fillMaxHeight(),
+
                 onDraw = {
                     val fromPoint = Offset(x = 0f, y = size.height.times(1 - fromValuePercentage))
-                    val toPoint =
-                        Offset(x = size.width, y = size.height.times(1 - tovaluePercentage))
+                    val toPoint = Offset(x = size.width, y = size.height.times(1 - tovaluePercentage))
 
                     drawLine(
                         color = lineColor,
                         start = fromPoint,
                         end = toPoint,
-                        strokeWidth = 3f
+                        strokeWidth = 10f
                     )
                 }
             )
@@ -80,9 +94,10 @@ val mockData = AssestInfo(
 
     )
 )
+/*
 
 @Composable
 @Preview
 fun PreviewChart(){
     PerfomanceChart()
-}
+}*/
