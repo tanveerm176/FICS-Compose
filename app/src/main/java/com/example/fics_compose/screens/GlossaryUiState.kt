05ee7a76c7bff3af6b1ea9a97485e0a96024f303
tanveerm176 <tanveerm176@gmail.com>
2024-01-30 22:15:18 -0500

    refactored glossary viewModel

--- app/src/main/java/com/example/fics_compose/screens/GlossaryUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.fics_compose.screens
-
-data class GlossaryUiState(
-    val showInformalDefinition: Boolean = false,
-    val showInformalLabel: Boolean  = false,
-    val expandedState: Boolean = false
-)
